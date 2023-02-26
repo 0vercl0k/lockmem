fd313d8df6aa96ba6271abccb4fbd7a07a9ff21a
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2023-02-25 17:44:24 -0800

    play w/ icl

--- src/lockmem/lockmem.cc
+++ src/lockmem/lockmem.cc
@@ -2,6 +2,7 @@
 #include <windows.h>
 
 #include <assert.h>
+#include <boost/icl/interval_set.hpp>
 #include <cstdint>
 #include <cstdio>
 #include <inttypes.h>
@@ -21,75 +22,25 @@
 #define dbgprintf(...) printf(__VA_ARGS__)
 #endif
 
+enum class THREADINFOCLASS : uint32_t { ThreadBasicInformation };
+
+extern "C" uint32_t NTAPI NtQueryInformationThread(
+    HANDLE ThreadHandle, THREADINFOCLASS ThreadInformationClass,
+    PVOID ThreadInformation, ULONG ThreadInformationLength,
+    PULONG ReturnLength);
+
 extern "C" NTSYSCALLAPI NTSTATUS NTAPI NtLockVirtualMemory(HANDLE ProcessHandle,
                                                            PVOID *BaseAddress,
                                                            PSIZE_T RegionSize,
                                                            ULONG MapType);
 
+using Interval_t = boost::icl::interval<uint64_t>;
+using Ranges_t = boost::icl::interval_set<uint64_t>;
+
 const uint64_t _1MB = 1'024 * 1'024;
 const NTSTATUS STATUS_WORKING_SET_QUOTA = 0xc000'00a1;
 const ULONG MAP_PROCESS = 1;
 const ULONG MAP_SYSTEM = 2;
-
-struct InclusiveRange_t {
-  uint64_t Start = 0;
-  uint64_t End = 0;
-  [[nodiscard]] uint64_t Size() const {
-    assert(End >= Start);
-    return End - Start + 1;
-  }
-
-  [[nodiscard]] std::optional<InclusiveRange_t>
-  Overlaps(const InclusiveRange_t &O) const {
-
-    //
-    // <> is the |O| range, [] is the |this| range.
-    //
-
-    //
-    // <-------[--->--------]
-    //
-
-    if (O.Start <= Start && O.End >= Start && O.End <= End) {
-      return InclusiveRange_t{Start, O.End};
-    }
-
-    //
-    // [-<-->--------]
-    //
-
-    if (O.Start >= Start && O.Start <= End && O.End <= End) {
-      return InclusiveRange_t{O.Start, O.End};
-    }
-
-    //
-    // [-------<---]-------->
-    //
-
-    if (O.Start >= Start && O.Start <= End && O.End >= End) {
-      return InclusiveRange_t{O.Start, End};
-    }
-
-    //
-    // <---[----------]--->
-    //
-
-    if (O.Start <= Start && O.End >= End) {
-      return InclusiveRange_t{Start, End};
-    }
-
-    return {};
-  }
-
-  static InclusiveRange_t Make(const PVOID Address, const size_t Size) {
-    InclusiveRange_t Range;
-    Range.Start = uint64_t(Address);
-    Range.End = Range.Start + Size + (Size > 0 ? -1 : 0);
-    return Range;
-  }
-};
-
-using Ranges_t = std::vector<InclusiveRange_t>;
 
 template <typename F_t> [[nodiscard]] auto finally(F_t &&f) noexcept {
   struct Finally_t {
@@ -108,8 +59,8 @@ template <typename F_t> [[nodiscard]] auto finally(F_t &&f) noexcept {
 
 [[nodiscard]] bool NT_SUCCESS(const NTSTATUS Status) { return Status >= 0; }
 
-std::optional<std::vector<InclusiveRange_t>> ParseRanges(std::string String) {
-  std::vector<InclusiveRange_t> Ranges;
+std::optional<Ranges_t> ParseRanges(std::string String) {
+  Ranges_t Ranges;
 
   //
   // Strip backticks if there's any... (WinDbg uses them to separate the lower
@@ -187,16 +138,16 @@ std::optional<std::vector<InclusiveRange_t>> ParseRanges(std::string String) {
     // All right, we have a range!
     //
 
-    Ranges.emplace_back(Start, End);
+    Ranges.insert(Interval_t::closed(Start, End));
   }
 
   return Ranges;
 }
 
 [[nodiscard]] bool GrownAndLockInWorkingSet(const HANDLE Process,
-                                            const InclusiveRange_t &Range) {
-  auto BaseAddress = PVOID(Range.Start);
-  auto RegionSize = SIZE_T(Range.Size());
+                                            const auto &Range) {
+  auto BaseAddress = PVOID(Range.lower());
+  auto RegionSize = boost::icl::size(Range);
   for (size_t Tries = 0; Tries < 10; Tries++) {
     const NTSTATUS Status =
         NtLockVirtualMemory(Process, &BaseAddress, &RegionSize, MAP_PROCESS);
@@ -269,47 +220,183 @@ std::optional<std::vector<InclusiveRange_t>> ParseRanges(std::string String) {
   return Pid;
 }
 
-[[nodiscard]] std::optional<InclusiveRange_t>
-RangesOverlap(const std::optional<Ranges_t> &Ranges, const PVOID BaseAddress,
-              const SIZE_T RegionSize) {
+[[nodiscard]] bool VirtRead(const uintptr_t RemoteAddress, void *Buffer,
+                            const size_t BufferLength,
+                            const HANDLE Process = GetCurrentProcess()) {
+  SIZE_T AmountRead = 0;
+  if (!ReadProcessMemory(Process, (void *)RemoteAddress, Buffer, BufferLength,
+                         &AmountRead)) {
+    return false;
+  }
+
+  return AmountRead == BufferLength;
+}
+
+template <typename Struct_t>
+[[nodiscard]] bool VirtRead(const uintptr_t RemoteAddress, Struct_t &Struct,
+                            const HANDLE Process = GetCurrentProcess()) {
+  return VirtRead(RemoteAddress, &Struct, sizeof(Struct), Process);
+}
+
+[[nodiscard]] std::optional<Ranges_t> GetStackRange(const HANDLE Process,
+                                                    const uint32_t Tid) {
 
   //
-  // If there's no range filter, we're good.
+  // Open the thread.
   //
 
-  const auto &RegionRange = InclusiveRange_t::Make(BaseAddress, RegionSize);
-  if (!Ranges) {
-    return RegionRange;
+  const HANDLE Thread = OpenThread(THREAD_QUERY_INFORMATION, FALSE, Tid);
+  if (Thread == nullptr) {
+    printf("OpenThread failed w/ GLE=%d\n", GetLastError());
+    return {};
+  }
+
+  const auto &CloseThread = finally([&] { CloseHandle(Thread); });
+
+  //
+  // Get the TEB.
+  //
+
+  struct {
+    uint32_t ExitStatus;
+    uintptr_t TebBaseAddress;
+    struct {
+      HANDLE UniqueProcess;
+      HANDLE UniqueThread;
+    } ClientId;
+    uintptr_t AffinityMask;
+    uint32_t Priority;
+    uint32_t BasePriority;
+  } ThreadInformation = {};
+
+  ULONG Length = 0;
+  const uint32_t Status = NtQueryInformationThread(
+      Thread, THREADINFOCLASS::ThreadBasicInformation, &ThreadInformation,
+      sizeof(ThreadInformation), &Length);
+  if (Status != 0 || Length != sizeof(ThreadInformation)) {
+    printf("NtQueryInformationThread failed w/ %x\n", Status);
+    return {};
   }
 
   //
-  // This is not ideal... but let's iterate until finding a filter that
-  // includes the range.
+  // Grab the TEB.
   //
 
-  for (const auto &Range : *Ranges) {
+  union {
+    struct {
+      uint64_t ExceptionList;
+      uint64_t StackBase;
+      uint64_t StackLimit;
+    } _64;
+    struct {
+      uint32_t ExceptionList;
+      uint32_t StackBase;
+      uint32_t StackLimit;
+    } _32;
+  } Tib = {};
+
+  if (!VirtRead(ThreadInformation.TebBaseAddress, Tib, Process)) {
+    printf("VirtRead failed, bailing\n");
+    return {};
+  }
+
+  Ranges_t StackRanges;
+#ifdef _WIN64
+  assert(Tib._64.StackLimit < Tib._64.StackBase && Tib._64.StackBase > 0);
+  StackRanges.insert(
+      Interval_t::right_open(Tib._64.StackLimit, Tib._64.StackBase));
+#else
+  assert(Tib._32.StackLimit < Tib._32.StackBase && Tib._32.StackBase > 0);
+  StackRanges.insert(
+      Interval_t::right_open(Tib._32.StackLimit, Tib._32.StackBase));
+#endif
+
+#ifdef _WIN64
+  uint16_t ProcessMachine = 0, NativeMachine = 0;
+  if (!IsWow64Process2(Process, &ProcessMachine, &NativeMachine)) {
+    printf("IsWow64Process2 failed w/ GLE=%d, bailing\n", GetLastError());
+    return {};
+  }
+
+  if (NativeMachine != IMAGE_FILE_MACHINE_AMD64) {
+    printf("NativeMachine has an unexpected value (%x), bailing\n",
+           NativeMachine);
+    return {};
+  }
+
+  if (ProcessMachine != IMAGE_FILE_MACHINE_I386) {
+    return StackRanges;
+  }
+
+  if (!VirtRead(Tib._64.ExceptionList, Tib, Process)) {
+    printf("VirtRead2 failed, bailing\n");
+    return {};
+  }
+
+  assert(Tib._32.StackLimit < Tib._32.StackBase && Tib._32.StackBase);
+  StackRanges.insert(
+      Interval_t::right_open(Tib._32.StackLimit, Tib._32.StackBase));
+#endif
+
+  return StackRanges;
+}
+
+[[nodiscard]] std::optional<Ranges_t> GetStackRanges(const HANDLE Process,
+                                                     const uint32_t Pid) {
+  Ranges_t Stacks;
+  HANDLE Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+  if (Snapshot == INVALID_HANDLE_VALUE) {
+    printf("CreateToolhelp32Snapshot failed w/ GLE=%d\n", GetLastError());
+    return {};
+  }
+
+  const auto &CloseSnapshot = finally([&] { CloseHandle(Snapshot); });
+
+  THREADENTRY32 Entry = {};
+  Entry.dwSize = sizeof(Entry);
+  if (!Thread32First(Snapshot, &Entry)) {
+    printf("Thread32First failed w/ GLE=%d\n", GetLastError());
+    return {};
+  }
+
+  do {
 
     //
-    // If the current range doesn't overlap with ours, try the next one.
+    // If it's not a thread owned by the process we want, continue.
     //
 
-    const auto &OverlappingRange = Range.Overlaps(RegionRange);
-    if (!OverlappingRange) {
+    if (Entry.th32OwnerProcessID != Pid) {
       continue;
     }
 
-    return OverlappingRange;
-  }
+    //
+    // Retrieve the stack ranges.
+    //
 
-  return {};
+    const auto &Ranges = GetStackRange(Process, Entry.th32ThreadID);
+    if (!Ranges) {
+      printf("GetStackRange failed for TID=%x, bailing\n", Entry.th32ThreadID);
+      return {};
+    }
+
+    for (const auto &Range : *Ranges) {
+      printf("TID %lu Stack Range: %" PRIx64 "-%" PRIx64 "\n",
+             Entry.th32ThreadID, Range.lower(), Range.upper());
+      Stacks.insert(Range);
+    }
+  } while (Thread32Next(Snapshot, &Entry));
+
+  return Stacks;
 }
 
 struct Opts_t {
   std::optional<std::string> NameOrPid;
   std::optional<Ranges_t> Ranges;
+  bool Stacks = false;
 };
 
 int main(int Argc, const char *Argv[]) {
+
   //
   // Parse arguments received.
   //
@@ -330,6 +417,8 @@ int main(int Argc, const char *Argv[]) {
         return EXIT_FAILURE;
       }
       Idx++;
+    } else if (Arg == "--stacks") {
+      Opts.Stacks = true;
     } else {
       Opts.NameOrPid = Arg;
     }
@@ -340,8 +429,8 @@ int main(int Argc, const char *Argv[]) {
   //
 
   if (!Opts.NameOrPid) {
-    printf(
-        "./lockme [--ranges 0-0x1000,0x2000+0x1000,..] <process name | pid>\n");
+    printf("./lockmem [--ranges 0-0x1000,0x2000+0x1000,..] [--stacks] <process "
+           "name | pid>\n");
     return EXIT_FAILURE;
   }
 
@@ -372,12 +461,37 @@ int main(int Argc, const char *Argv[]) {
                   false, ProcessId);
 
   if (!Process) {
-    printf("Failed to OpenProcess(%s) w/ GLE=%d, exiting.\n", Argv[1],
+    printf("OpenProcess(%s) failed w/ GLE=%d, exiting.\n", Argv[1],
            GetLastError());
     return EXIT_FAILURE;
   }
 
   const auto &CloseProcess = finally([&] { CloseHandle(Process); });
+
+  //
+  // Get threads stacks.
+  //
+
+  std::optional<Ranges_t> AllowRanges;
+  if (Opts.Stacks) {
+    AllowRanges = GetStackRanges(Process, ProcessId);
+    if (!AllowRanges) {
+      printf("GetStacks failed, exiting\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  //
+  // Get the user ranges in there as well.
+  //
+
+  if (Opts.Ranges) {
+    if (AllowRanges) {
+      *AllowRanges += *Opts.Ranges;
+    } else {
+      AllowRanges = *Opts.Ranges;
+    }
+  }
 
   //
   // Iterate through memory ranges of the process.
@@ -399,7 +513,7 @@ int main(int Argc, const char *Argv[]) {
     const auto RegionSize = MemoryInfo.RegionSize;
     const uint32_t BadProtectBits = PAGE_GUARD | PAGE_NOACCESS;
     if (MemoryInfo.Protect & BadProtectBits) {
-      dbgprintf("Skipping %p - %llx because of protect bad bits..\n",
+      dbgprintf("Skipping %p - %zx because of protect bad bits..\n",
                 BaseAddress, RegionSize);
       continue;
     }
@@ -410,7 +524,7 @@ int main(int Argc, const char *Argv[]) {
 
     const uint32_t BadStatetBits = MEM_FREE | MEM_RESERVE;
     if (MemoryInfo.State & BadStatetBits) {
-      dbgprintf("Skipping %p - %llx because of state bad bits..\n", BaseAddress,
+      dbgprintf("Skipping %p - %zx because of state bad bits..\n", BaseAddress,
                 RegionSize);
       continue;
     }
@@ -419,44 +533,51 @@ int main(int Argc, const char *Argv[]) {
     // Check if the region overlaps with a filter.
     //
 
-    const auto &OverlappingRange =
-        RangesOverlap(Opts.Ranges, BaseAddress, RegionSize);
-    if (!OverlappingRange) {
-      dbgprintf("Skipping %p - %llx because not in ranges..\n", BaseAddress,
-                RegionSize);
-      continue;
+    const auto &RegionRange = Interval_t::right_open(
+        uint64_t(BaseAddress), uint64_t(BaseAddress) + RegionSize);
+    Ranges_t OverlappingRanges;
+    OverlappingRanges.insert(RegionRange);
+    if (AllowRanges) {
+      OverlappingRanges = *AllowRanges & RegionRange;
     }
 
-    //
-    // Extend the WS if needed, and lock the region in.
-    //
+    for (const auto &OverlappingRange : OverlappingRanges) {
 
-    if (!GrownAndLockInWorkingSet(Process, *OverlappingRange)) {
-      printf("GrownAndLockInWorkingSet failed, exiting.\n");
-      return EXIT_FAILURE;
+      //
+      // Extend the WS if needed, and lock the region in.
+      //
+
+      if (!GrownAndLockInWorkingSet(Process, OverlappingRange)) {
+        printf("GrownAndLockInWorkingSet failed, exiting.\n");
+        return EXIT_FAILURE;
+      }
+
+      const auto OverlappingStart = PVOID(OverlappingRange.lower());
+      const auto OverlappingSize = boost::icl::size(OverlappingRange);
+      dbgprintf("Locked %p (%lld MB) in memory..\r", OverlappingStart,
+                OverlappingSize / _1MB);
+#if 0
+      auto Buffer = std::make_unique<uint8_t[]>(OverlappingSize);
+      SIZE_T NumberBytesRead = 0;
+      const bool Ret =
+          ReadProcessMemory(Process, OverlappingStart, Buffer.get(),
+                            OverlappingSize, &NumberBytesRead);
+      if (!Ret || NumberBytesRead != OverlappingSize) {
+        printf("ReadProcessMemory failed w/ GLE=%d, exiting.\n",
+               GetLastError());
+        return EXIT_FAILURE;
+      }
+#endif
+
+      //
+      // OK we're done.
+      //
+
+      dbgprintf("Read region %p..\r", OverlappingStart);
+      NumberBytes += OverlappingSize;
+      AmountMb = NumberBytes / _1MB;
     }
 
-    const auto OverlappingStart = PVOID(OverlappingRange->Start);
-    const auto OverlappingSize = SIZE_T(OverlappingRange->Size());
-    dbgprintf("Locked %p (%lld MB) in memory..\r", OverlappingStart,
-              OverlappingSize / _1MB);
-
-    auto Buffer = std::make_unique<uint8_t[]>(OverlappingSize);
-    SIZE_T NumberBytesRead = 0;
-    const bool Ret = ReadProcessMemory(Process, OverlappingStart, Buffer.get(),
-                                       OverlappingSize, &NumberBytesRead);
-    if (!Ret || NumberBytesRead != OverlappingSize) {
-      printf("ReadProcessMemory failed w/ GLE=%d, exiting.\n", GetLastError());
-      return EXIT_FAILURE;
-    }
-
-    //
-    // OK we're done.
-    //
-
-    dbgprintf("Read region %p..\r", OverlappingStart);
-    NumberBytes += OverlappingSize;
-    AmountMb = NumberBytes / _1MB;
     printf("Locked %llu MBs..\r", AmountMb);
   }
 
